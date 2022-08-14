32ed5534269072da1be708c8d7b6bf32ff5725d1
BinhNguyen1234 <88345612+BinhNguyen1234@users.noreply.github.com>
2022-08-14 10:47:22 +0700

    done upload blog and save into post collection and user collection

--- controller/login/auth/writeblog/index.js
+++ controller/login/auth/writeblog/index.js
@@ -22,54 +22,63 @@ const user = require ('../../../../model/user')
 
 async function  postBlog (req,res){
     let writer;
+    let post;
     try{
     writer = await user.findOne({"username": req.user.username})
     console.log('writer', writer)
     }catch{(e)=>{
-        console.log('have error  when find user to write blog\n', err)
+        console.log('have error  when find user to write blog\n', err);
     }}
      
-    const post = new postModel({
-        '_writter': writer._id,
-        'title' : req.body.data.title,
-        'content': req.body.data.content,
-        'date': new Date()
-    })
+   
     const compareTitle =  new Promise((resolve, reject)=>{
         postModel.findOne({
             'title' : req.body.data.title
         },(err,sucess)=>{
             if (err){
-                reject(err)
+                reject(err);
             }else{
-                resolve(sucess)
+                resolve(sucess);
             }
         })
     })
 
     await compareTitle.then( (sucess=>{
         if(!sucess){
-            post.isNew = true
-            console.log("title not duplicate")
+            
+            console.log(`title not duplicate(${req.body.data.title})`)
+            post = new postModel({
+                '_writter': writer._id,
+                'title' : req.body.data.title,
+                'content': req.body.data.content,
+                'date': new Date()
+            })
+            post.save()
+            .then(()=>{
+                console.log('content',req.body.data.content,)
+                console.log("save into db")
+                writer.updateOne({$push: {_post: post._id}},(err, result)=>{if(err){
+                    console.log("error when updated post to writer",err)
+                }else{
+                    console.log('save new post to writer sucess', result)
+                }
+                })
+                res.sendStatus(206);
+            })
+            .catch((err)=>{
+                res.sendStatus(406)
+                console.log('cannot save into db', err)
+            })
+            
         }
         else{
-            post.isNew = false
+            res.sendStatus(406)
             console.log("title duplicate")
         }
     }))
     .catch((err)=>{
         console.log("error when compare Title",err)
     })
-    console.log(post.isNew)
-    console.log(post)
-    post.save()
-    .then(()=>{
-        console.log("save into db")
-        res.sendStatus(206);
-    })
-    .catch((err)=>{
-        res.sendStatus(406)
-        console.log('cannot save into db', err)
-    })
+   
 }
 module.exports = postBlog;
--- index.js
+++ index.js
@@ -22,8 +22,9 @@ const cors = require('cors')
 app.use(cors())
 
 Database.connect('binhnguyen','170116Abc','Blog')
-app.use(bodyParser.json({limit: '5mb'}));
-app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
+//-- limit 16mb for document in mongodb
+app.use(bodyParser.json({limit: '15mb'}));
+app.use(bodyParser.urlencoded({limit: '15mb', extended: true}));
 app.listen(PORT,()=>{
     console.log(`Server running at ${PORT} \n -------------------- `);
 })
